0537eb018130ddbffbf9377c9979aedbd2c945aa
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-18 20:55:42 +0700

    Modified:
    - Refactor code

--- Networks/Main.py
+++ Networks/Main.py
@@ -24,7 +24,7 @@ def RBM():
     #########################################
     # Load dataset from local disk or download from internet
     datasets = DataHelper.LoadData(DATASET_NAME)
-    trainSetX = datasets[0]
+    trainSetX = datasets[0][0]
     nTrains = trainSetX.get_value(borrow = True).shape[0]
     nTrainBatchs = nTrains // BATCH_SIZE
 
@@ -42,26 +42,34 @@ def RBM():
 
     # Create shared variable for input
     Index = T.lscalar('Index')
+    LearningRate = T.scalar('LearningRate', dtype = 'float32')
     X = T.matrix('X')
+    X = X.reshape((BATCH_SIZE, 28 * 28))
 
     # Construct the RBM class with one hidden layer
+    persistentChain = theano.shared(
+                            numpy.zeros((BATCH_SIZE, NUM_HIDDEN), dtype=theano.config.floatX),
+                            borrow=True
+                    )
     rbm = RBMHiddenLayer(
-        rng = rng,
-        theanoRng = theanoRng,
-        input = X,
-        numVisible = 28 * 28,
-        numHidden = NUM_HIDDEN
+        rng          = rng,
+        theanoRng    = theanoRng,
+        input        = X,
+        numVisible   = 28 * 28,
+        numHidden    = NUM_HIDDEN,
+        learningRate = LearningRate,
+        persistent   = persistentChain,
+        kGibbsSample = 15
     )
 
     #########################################
     #      CREATE TRAIN FUNCTION            #
     #########################################
-
     # Cost function
     trainFunc = theano.function(
-        inputs  = [Index],
-        Outputs = [minotoringCost],
-        updates = updates,
+        inputs  = [Index, LearningRate],
+        outputs = [rbm.MonitoringCost],
+        updates = rbm.Updates,
         givens  = {
             X: trainSetX[Index * BATCH_SIZE : (Index + 1) * BATCH_SIZE]
         }
@@ -70,11 +78,10 @@ def RBM():
     #########################################
     #      TRAIN THE MODEL                  #
     #########################################
-
     for epoch in range(NUM_EPOCHS):
         meanCost = []
         for batchIndex in range(nTrainBatchs):
-            cost = trainFunc(Index)
+            cost = trainFunc(batchIndex, LEARNING_RATE)
             meanCost.append(cost)
 
         print ('Epoch = %d, cost = %f' % (epoch, numpy.mean(meanCost)))
@@ -89,133 +96,6 @@ def RBM():
             )
         )
         image.save('filters_at_epoch_%i.png' % (epoch))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # # Create storage for the persistance chain
-    # persistanceChain = theano.shared(
-    #     numpy.zeros(
-    #         (BATCH_SIZE, NUM_HIDDEN),
-    #         dtype = theano.config.floatX
-    #     ),
-    #     borrow = True
-    # )
-
-
-    # Get the cost and the gradient corresponding to one step of CD-15
-    cost, updates = rbm.GetCostUpdates(
-        learningRate = LEARNING_RATE,
-        persistent = persistanceChain,
-        k = 15
-    )
-
-    #############################
-    #     Training the RBM      #
-    #############################
-    trainRBM = theano.function(
-        inputs = [Index],
-        outputs = [cost],
-        updates = updates,
-        givens = {
-            X: trainSetX[Index * BATCH_SIZE : (Index + 1) * BATCH_SIZE]
-        }
-    )
-
-    plottingTime = 0
-
-    # Training............
-    if __name__ == '__main__':
-        for epoch in range(NUM_EPOCHS):
-            meanCost = []
-            for batchIndex in range(nTrainBatchs):
-                meanCost += [trainRBM(batchIndex)]
-
-            print('Training epoch = %d, cost is %f ' % (epoch, numpy.mean(meanCost)))
-
-            image = Image.fromarray(
-                tile_raster_images(
-                    X = rbm.W.get_value(borrow = True).T,
-                    img_shape = (28, 28),
-                    tile_shape = (10, 10),
-                    tile_spacing = (1, 1)
-                )
-            )
-            image.save('filters_at_epoch_%i.png' % (epoch))
-
-    # Testing...............
-    # Pick random test examples
-    testIdx = rng.randint(nTests - NUM_CHAINS)
-    persistentVisChain = theano.shared(
-        numpy.asarray(
-            testSetX.get_value(borrow = True)[testIdx : testIdx + NUM_CHAINS],
-            dtype = theano.config.floatX
-        )
-    )
-
-    plotEvery = 1000
-    (
-        [
-            presigHigs,
-            hidMfs,
-            hidSamples,
-            presigVis,
-            visMfs,
-            visSamples
-        ],
-        updates
-    ) = theano.scan(
-        rbm.GibbsVhv,
-        outputs_info = [None, None, None, None, None, persistentVisChain],
-        n_steps = plotEvery
-    )
-
-    updates.update({persistentVisChain: visSamples[-1]})
-
-    sampleFn = theano.function(
-        [],
-        [
-            visMfs[-1],
-            visSamples[-1]
-        ],
-        updates = updates
-    )
-
-    imageData = numpy.zeros(
-        (29 * NUM_SAMPLES + 1, 29 * NUM_CHAINS - 1),
-        dtype = 'uint8'
-    )
-    for idx in range(NUM_SAMPLES):
-        visMf, visSample = sampleFn()
-        print('... plotting sample %d' %idx)
-        imageData[29 * idx : 29 * idx + 28, :] = tile_raster_images(
-            X = visMf,
-            img_shape = (28, 28),
-            tile_shape = (1, NUM_CHAINS),
-            tile_spacing = (1, 1)
-        )
-
-    image = Image.fromarray(imageData)
-    image.save('sample.png')
 
 if __name__ == '__main__':
     RBM()
